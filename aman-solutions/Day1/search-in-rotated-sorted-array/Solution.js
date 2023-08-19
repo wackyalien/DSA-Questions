2943f97e2be920bd49e44df7f87072f9c951eb17
Aman Jain <amanjain7838@gmail.com>
2023-08-19 16:02:33 +0530

    optimised solution

--- aman-solutions/Day1/search-in-rotated-sorted-array/Solution.js
+++ aman-solutions/Day1/search-in-rotated-sorted-array/Solution.js
@@ -5,11 +5,33 @@
  */
  var search = function(nums, target) {
     let index=-1;
-    for(let i=0;i<nums.length;i++) {
-        if(nums[i] == target){
-            index=i;
+    // for(let i=0;i<nums.length;i++) {
+    //     if(nums[i] == target){
+    //         index=i;
+    //         break;
+    //     }
+    // }
+    // return index;
+    if(nums[0] == target )
+        return 0;
+    else if(nums[1] == target)
+        return 1;
+    
+    let low = 0;
+    let high = nums.length-1; 
+    let counter = 0;
+    while(low<=high) {
+        let mid = low + (high - low)/2;
+        if(nums[mid] >= target)
+            high = mid;
+        else 
+            low = mid;
+        if(counter++>10)
+        {
+            System.out.print("force break");
             break;
         }
     }
-    return index;
+    return mid;
 };
+console.log(search([4,5,6,7,0,1,2],0))
--- aman-solutions/Day1/square-root-integral_893351/Solution.java
+++ aman-solutions/Day1/square-root-integral_893351/Solution.java
@@ -8,17 +8,24 @@ public class Solution {
 		 * Write your code here
 		 */
 		int answer=-1;
-		for(int i=1;i<N;i++) {
-			int nextnumber = (i+1);
-			if((i*i)==N || 
-				((nextnumber)*(nextnumber)>N) 
-				&& ((i*i)<=N)
-			){
-				answer=i;
-				break;
-			}
+		if(N == 1 || N == 0)
+			return (int)N;
+		
+		int low = 0;
+		int high = (int) N;
+		int mid;
+
+		while(low<high) {
+
+			mid = low + ((high-low)/2);
+
+			if(mid <= (N/mid))
+				low = mid + 1 ;
+			else
+				high = mid;
+
 		}
-		return answer;
+		return (int) low -1;
 	}
     public static void main(String args[]){
         int answer=sqrtN(6);
