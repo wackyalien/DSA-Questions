1440ee9e8cb0f5a57df0ab7aea8b13636b891d0d
Aman Jain <amanjain7838@gmail.com>
2023-09-06 02:22:50 +0530

    optimised solution

--- aman-solutions/Day4/ayush-gives-ninjatest_1097574/Solution.java
+++ aman-solutions/Day4/ayush-gives-ninjatest_1097574/Solution.java
@@ -1,16 +1,52 @@
 import java.util.* ;
 import java.io.*; 
 public class Solution {
-    public static long ayushGivesNinjatest(int n, int m, int[] time) {
+    public static long ayushGivesNinjatest(int nodays, int m, int[] time) {
         // Write your code here.
-        double _setlength = (m*1.0)/n;
-        int subsetlength =(int) Math.ceil(_setlength);
+
+        int high=0,max=time[0];
+        for(int i = 0;i<time.length;i++){
+            high+=time[i];
+            if(max<time[i])
+                max=time[i];
+        }
+        int low = max;
+
+        int answer=0;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            int nodaysrequired = calculatedays(time,mid);
+            // System.out.println(" mid "+mid+" nodaysrequired "+nodaysrequired + " low " + low + " high "+high+ " answer "+answer);
+
+            if(nodaysrequired==nodays)
+                answer=mid;
+
+            if(nodaysrequired<=nodays)
+                high=mid-1;
+            else
+                low = mid+1;
+        }
 
 
         
-        return maxlength;
+        return low;
     }
 
+    public static int calculatedays(int[] time,int maxstudyfordayallowed ){
+
+        int days = 1,maxstudiedinday=0;
+        for(int i = 0;i<time.length;i++){
+            if(maxstudiedinday+time[i]<=maxstudyfordayallowed){
+                maxstudiedinday+=time[i];
+            }
+            else{
+                days++;
+                maxstudiedinday=time[i];
+            }
+        }
+        return days;
+    }
     
 
 
